88e283857586d789ef1ffdccfe3800609480c1ca
Christian Lura <christian.lura@bt.no>
2014-03-02 17:17:15 -0500

    Vastly improved
    Whole new algorithm is faster. Checks also how many times the search
    string is found in the text.

--- tekst.R
+++ tekst.R
@@ -1,7 +1,8 @@
 ## Skript for å lete gjennom tekst og finne match
 
 # Hent inn kildetekst fra filen text.txt
-kildetekst <- scan(file = "text.txt", what = 'character')
+tekst <- scan(file = "text.txt", what = 'character')
+kildetekst <- tekst
 
 # Få kildeteksten inn i én streng.
 kildetekst <- paste(kildetekst, sep = " ", collapse = " ")
@@ -16,30 +17,40 @@ if (inputtekst=="") source('tekst.R')
 lengde_kildetekst <- nchar(kildetekst)
 lengde_inputtekst <- nchar(inputtekst)
 
-# Hvor lang er inputtekst i forhold til kildetekst?
+# Hvor lang er inputtekst i forhold til kildetekst? Prosentregning
 input_vs_kilde <- round(100 * lengde_inputtekst / lengde_kildetekst, digits=2)
-
-#Ordanalyse:
-#Splitt kildetekst i sine minste bestanddeler. Sett den sammen igjen
-#bokstav for bokstav og legg alt i en liste.
-atom <- unlist(strsplit(kildetekst, ""))
-biter <- list()
-lengde <- length(atom)
-    for (i in 1:lengde) {
-        biter <- c(biter, paste(atom[1:i], collapse=""), paste(atom[i:lengde], collapse=""))
-    }
-       
+   
+#Funksjon for ordanalyse.
+#Lag en liste av kildetekst der første element er fra første bokstav til lengde_inputtekst.
+#Gå deretter gjennom kildetekst ved å flytte en plass mot høyre i hver loop.
+ord <- list()
+    sjekk <- function(x) {
+        diff <- lengde_kildetekst-x+1
+        for (i in 1:diff) {
+            ord <- c(ord, paste(atom[i:(i+(x-1))], collapse=""))
+                }
+                return(ord)
+        }
+   
 #Finnes teksten vi leter etter i listen biter?
 #Tell opp alle forekomster der hele inputtekst er i listen.
-res <- grepl(inputtekst, biter)
-res2 <- length(res[res==TRUE])
+res <- grepl(inputtekst, kildetekst)
+
+#Ordanalyse: Sjekk hvor mange ganger strengen dukker opp i kildeteksten
+#Regn ut omfanget søkestrengen har i teksten: Prosent av lengde på kildetekst * antall treff.
+ordliste <- sjekk(lengde_inputtekst)
+treff <- length(which(ordliste == inputtekst))
+omfang <- input_vs_kilde * treff
+
+#Tekster til rapport
+ja <- 'Teksten du søker etter, finnes i kildeteksten.\n'
+nei <- 'Teksten du søker etter, finnes ikke i kildeteksten.\n\n'
+svar1 <- paste(dQuote(inputtekst),'utgjør',omfang,'% av kildeteksten.\n', collapse="")
+svar2 <- paste('Søketeksten finnes', treff, 'steder i kildeteksten.\n\n', collapse="")
 
 #Sjekk om inputtekst faktisk er i kildetekst.
-#Hvis res2 >0, finnes inputteksten i kildeteksten.
-# input_vs_kilde forteller hvor godt inputteksten matcher kildeteksten.
-ja <- 'Teksten du søker etter, finnes i kildeteksten.\n'
-nei <- 'Teksten du søker etter, finnes ikke i kildeteksten.\n'
-svar <- paste(dQuote(inputtekst),'utgjør',input_vs_kilde,'% av kildeteksten.', collapse="")
+#Hvis res=TRUE, finnes inputteksten i kildeteksten.
 
-if (res2>0) paste(cat(ja,svar)) else cat(nei)
+if (res==TRUE) paste(cat(ja,svar1,svar2)) else cat(nei)
+
 source('tekst.R')
