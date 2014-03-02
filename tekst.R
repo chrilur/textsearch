14791bc07737b7a8ad0ee56b202f4a2168b7f1c8
Christian Lura <christian.lura@bt.no>
2014-03-02 12:24:25 -0500

    Changed for-loop
    Tried to make the code more efficient. Now checks for length of text
    string to determine the relevance.

--- tekst.R
+++ tekst.R
@@ -1,40 +1,44 @@
 ## Skript for å lete gjennom tekst og finne match
 
-# To teksteksempler
-tekst <- scan(file = "text.txt", what = 'character')
-tekst2 <- readline("Skriv tekst, minst to bokstaver: ")
+# Hent inn kildetekst fra filen text.txt
+kildetekst <- scan(file = "text.txt", what = 'character')
 
-# Få tekst inn i én streng.
-tekst <- paste(tekst, sep = " ", collapse = " ")
-#Splitte strengen i sine minste bestanddeler. Sett den sammen igjen
+# Få kildeteksten inn i én streng.
+kildetekst <- paste(kildetekst, sep = " ", collapse = " ")
+
+# Spør etter tekst som skal sjekkes mot kildetekst
+inputtekst <- readline("Skriv søketekst: ")
+
+if (inputtekst=="") source('tekst.R') 
+
+# Hvor lange er kildetekst og inputtekst?
+lengde_kildetekst <- nchar(kildetekst)
+lengde_inputtekst <- nchar(inputtekst)
+
+# Hvor lang er inputtekst i forhold til kildetekst?
+input_vs_kilde <- round(100 * lengde_inputtekst / lengde_kildetekst, digits=2)
+
+#Ordanalyse:
+#Splitt kildetekst i sine minste bestanddeler. Sett den sammen igjen
 #bokstav for bokstav og legg alt i en liste.
-
-atom <- unlist(strsplit(tekst, ""))
+atom <- unlist(strsplit(kildetekst, ""))
 biter <- list()
 lengde <- length(atom)
     for (i in 1:lengde) {
-        biter <- c(biter, paste(atom[1:i], collapse=""))
+        biter <- c(biter, paste(atom[1:i], collapse=""), paste(atom[i:lengde], collapse=""))
     }
-    for (i in 1:lengde) {
-        biter <- c(biter, paste(atom[i:lengde], collapse=""))
-    }
-deler <- length(biter)/2
-   
-#Finnes teksten vi leter etter i liste?
-#Tell opp alle forekomster der hele tekst2 er i listen.
-res <- grepl(tekst2, biter)
+       
+#Finnes teksten vi leter etter i listen biter?
+#Tell opp alle forekomster der hele inputtekst er i listen.
+res <- grepl(inputtekst, biter)
 res2 <- length(res[res==TRUE])
 
-##Hvis det er ingen forekomster, skal svare være 0%
-## Hvis det er to forekomster, skal svare være 100%
-## ifelse(<condition>,ifelse(<condition>,<yes>,<no>),<no>)
+#Sjekk om inputtekst faktisk er i kildetekst.
+#Hvis res2 >0, finnes inputteksten i kildeteksten.
+# input_vs_kilde forteller hvor godt inputteksten matcher kildeteksten.
+ja <- 'Teksten du søker etter, finnes i kildeteksten.\n'
+nei <- 'Teksten du søker etter, finnes ikke i kildeteksten.\n'
+svar <- paste(dQuote(inputtekst),'utgjør',input_vs_kilde,'% av kildeteksten.', collapse="")
 
-resultat <- ifelse(res2 !=2 , ifelse(res2 !=0, deler-res2, 0),deler)
-
-# Regner ut prosentvis hvor godt tekstene stemmer overens.
-# deler = 100%
-# resultat = ?%
-
-svar <- 100 * resultat / deler
-
-paste(cat(dQuote(tekst2),'samsvarer',svar,'% med teksten.'), collapse="")
+if (res2>0) paste(cat(ja,svar)) else cat(nei)
+source('tekst.R')
